c5070fa15e217734ead6cacdee2e2898139cd78a
cat2746 <emanuel6822@gmail.com>
2024-11-25 10:36:43 -0800

    h

--- webroot/js/config.js
+++ webroot/js/config.js
@@ -1,8 +1,8 @@
 //Some of the settings may or may not work as this is not a fully completed update. Mainly Appearance settings and weather variables that arent finished.
 //Input API Keys below. If no API is inputted, sim will default to no report.
 var api_key = 'e1f10a1e78da46f5b10a1e78da96f525';
-var map_key = 'YOUR_API_KEY';
-var traf_key = 'YOUR_API_KEY';
+var map_key = 'pk.eyJ1Ijoid2VhdGhlciIsImEiOiJjaW4wbzllcjkwYWtrd2JsdWl3dG1hZjZ1In0.AdcjoWQiVfvjHfOB0nsyeQ';
+var traf_key = 'e7l1ZkbztmkqTdRNWRACuI7NzWgE0YCr';
 
 //Apperance settings. Fields left blank will use defaults. Will only refresh upon reload.
 var apperanceSettings = {
